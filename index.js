b2137f0075328bfa716305451f759347b5168787
luck22y <luckykumar2204@gmail.com>
2023-06-14 14:11:59 +0530

    add:views

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//----------- Importing Modules -----------//
 const express = require("express");
 const app = express();
 const port = 3000;
@@ -10,34 +11,41 @@ const session = require("express-session");
 const db = require("./config/mongoose");
 
 //-----EJS---------//
-app.use(expressLayouts)
-app.use('/assets',express.static('/assets'))
-app.set('view engine', 'ejs')
+app.use(expressLayouts);
+app.use("/assets", express.static("/assets"));
+app.set("view engine", "ejs");
 app.set("views", "./views");
 
 //------BodyParser--------//
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 
 //---------Connect Flash----------//
-app.use(flash())
+app.use(flash());
 
 //---------Express Session----------//
 app.use(
-    session({
-        secret:'secret',
-        resave:true,
-        saveUninitialized:true
-    })
-)
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 //---------Global Variables----------//
-
-
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 //-----Routes---------//
-app.use
+app.use("/", require("./routes/index.js"));
+app.use("/users", require("./routes/users"));
 
-app.listen(port, function(err){
-    if(err){ console.log(`Error starting the server`)}
-    console.log(`Server up & running on PORT:`, port)
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error starting the server`);
+  }
+  console.log(`Server up & running on PORT:`, port);
 });
